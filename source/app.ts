ab71cb72a5761b4add56c7b0e99b1261c58e1f25
Mitka Kuznetsov <www.kuznetsovsky@gmail.com>
2024-01-08 16:04:08 +0300

    ref: put all routes into a file with routes

--- source/app.ts
+++ source/app.ts
@@ -1,10 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
-import swaggerUi from 'swagger-ui-express'
-import { StatusCodes } from 'http-status-codes'
-import type { NextFunction, Request, Response } from 'express'
 
-import swaggerDocument from '../docs'
 import { ENV, IS_DEV } from '../config'
 import { appRoutes } from './routes'
 import { expressSession } from './session'
@@ -31,19 +27,4 @@ if (ENV !== 'test') {
   app.use(morgan(IS_DEV ? 'dev' : 'combined'))
 }
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-
-app.get('/', (req: Request, res: Response): void => {
-  res.send({ message: 'API is working, change to version /api/v1' })
-})
-
 appRoutes(app)
-
-app.use((req: Request, res: Response, next: NextFunction): void => {
-  res.status(StatusCodes.NOT_FOUND).end()
-})
-
-app.use((err: any, req: Request, res: Response, next: NextFunction): void => {
-  console.error(err)
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).end()
-})
--- source/routes.ts
+++ source/routes.ts
@@ -1,6 +1,15 @@
-import { type Application } from 'express'
+import swaggerUi from 'swagger-ui-express'
+import { StatusCodes } from 'http-status-codes'
+
+import type {
+  NextFunction,
+  Request,
+  Response,
+  Application
+} from 'express'
 
 import { VERSION } from '../config'
+import swaggerDocument from '../docs'
 import { isAuthorized } from './common/helpers/is-authorized.helper'
 
 import { router as authRouter } from './auth/auth.router'
@@ -9,8 +18,26 @@ import { router as usersRouter } from './users/users.router'
 import { router as feedbackRouter } from './feedback/feedback.router'
 
 export function appRoutes (app: Application): void {
+  // Before
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+  app.get('/', (req: Request, res: Response): void => {
+    res.send({ message: 'API is working, change to version /api/v1' })
+  })
+
+  // Routes
   app.use(`${VERSION}/auth`, authRouter)
   app.use(`${VERSION}/user`, isAuthorized, userRouter)
   app.use(`${VERSION}/users`, isAuthorized, usersRouter)
   app.use(`${VERSION}/feedback`, isAuthorized, feedbackRouter)
+
+  // Affter
+  app.use((req: Request, res: Response, next: NextFunction): void => {
+    res.status(StatusCodes.NOT_FOUND).end()
+  })
+
+  app.use((err: any, req: Request, res: Response, next: NextFunction): void => {
+    console.error(err)
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).end()
+  })
 }
