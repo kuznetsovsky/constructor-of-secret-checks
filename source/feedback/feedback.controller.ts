14e5a16695166679111e95841ac3ec98d3781fde
Mitka Kuznetsov <www.kuznetsovsky@gmail.com>
2024-01-09 11:33:01 +0300

    feat: create send feedback by email

--- source/feedback/feedback.controller.ts
+++ source/feedback/feedback.controller.ts
@@ -1,5 +1,11 @@
 import { StatusCodes } from 'http-status-codes'
+import sanitizer from 'sanitize-html'
+
+import { knex } from '../connection'
+import { FEEDBACK_EMAIL } from '../../config'
 import { type Feedback } from './feedback.interface'
+import { sendMail } from '../common/libs/nodemailer.lib'
+import { AccountRepository } from '../common/repositories/account.repository'
 
 import type {
   Request,
@@ -12,17 +18,43 @@ export async function sendFeedback (
   res: Response,
   next: NextFunction
 ): Promise<void> {
-  const { message } = req.body
+  const accountRepository = new AccountRepository(knex, 'accounts')
 
-  // TODO:
-  // 1. send a message by email
-  // 2. make restrictions on sending spam messages
-  console.log('Feedback', message)
+  const { message } = req.body
+  const ID = req.session.user?.id
+
+  if (ID == null) {
+    const error = new Error('ID is missing.')
+    next(error)
+    return
+  }
 
   try {
-    res
-      .status(StatusCodes.OK)
-      .end()
+    const account = await accountRepository.findOne(ID, ['email'])
+
+    if (account == null) {
+      const error = new Error('account is missing.')
+      next(error)
+      return
+    }
+
+    // TODO:
+    // 2. make restrictions on sending spam messages
+
+    const sanitizeMessage = sanitizer(message)
+    const sendingMailStatus = await sendMail(account.email, FEEDBACK_EMAIL, 'User feedback', sanitizeMessage)
+
+    if (sendingMailStatus) {
+      res
+        .status(StatusCodes.OK)
+        .end()
+    } else {
+      res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({
+          error: 'Failed to send message'
+        })
+    }
   } catch (error) {
     next(error)
   }
