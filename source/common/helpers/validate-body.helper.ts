06edaa30de34d6cfd075dc03191b122428ea0e2a
Mitka Kuznetsov <www.kuznetsovsky@gmail.com>
2024-01-19 15:25:06 +0300

    ref: add type field for validate-body

--- source/common/helpers/validate-body.helper.ts
+++ source/common/helpers/validate-body.helper.ts
@@ -3,12 +3,13 @@ import type { ValidateFunction } from 'ajv'
 import type { Request, Response, NextFunction } from 'express'
 
 export function validateBody (validate: ValidateFunction) {
-  return function bodyValidateMiddleware (req: Request, res: Response, next: NextFunction): void {
+  return function _validateBody (req: Request, res: Response, next: NextFunction): void {
     if (!validate(req.body)) {
       res
         .status(StatusCodes.UNPROCESSABLE_ENTITY)
         .json({
-          message: 'Validation failed',
+          type: 'body',
+          message: 'Validation failed.',
           errors: validate.errors
         })
 
